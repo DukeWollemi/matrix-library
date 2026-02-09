871dbad91eb956ca450debcd6d0b28607ee1c5e6
DukeWollemi <omarelshora396@gmail.com>
2026-02-09 10:51:04 +0000

    Added a test smoke to ensure environment and pipeline stability.

--- tests/test_matrix.cpp
+++ tests/test_matrix.cpp
@@ -4,3 +4,10 @@
 
 #include <gtest/gtest.h>
 #include "matrix/Matrix.h"
+
+// A test smoke to make sure a Matrix can be contructed
+TEST(MatrixContruct, CanBeConstructed) {
+    //This test passes if the constructor and destructor can be called without crashing or throwing an unhandled exception.
+    Matrix<int> matrix{2, 6};
+    SUCCEED(); // Explicitly mark the test as successful.
+}
