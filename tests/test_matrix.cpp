0edcf5a292556a92ba295901e0bbbb165cdee0a8
DukeWollemi <omarelshora396@gmail.com>
2026-03-11 09:49:47 +0000

    Added tests to different constructor for matrix.

--- tests/test_matrix.cpp
+++ tests/test_matrix.cpp
@@ -7,12 +7,20 @@
 
 // Default constructor
 
-TEST(MatrixDefaultConstructor, ProducesEmptyMatrix) {
-    Matrix<int> m;
-    EXPECT_EQ(m.rows(), 0u);
-    EXPECT_EQ(m.cols(), 0u);
-    EXPECT_EQ(m.size(), 0u);
-    EXPECT_TRUE(m.empty());
-    EXPECT_EQ(m.data(), nullptr);
+// A test smoke to make sure a Matrix can be contructed
+TEST(MatrixContruct, CanBeConstructed) {
+    //This test passes if the constructor and destructor can be called without crashing or throwing an unhandled exception.
+    Matrix<int> matrix{ 2, 6 };
+    
+	// Testing with empty init list.
+	Matrix<double> d_matrix{};
+
+    // Testing default constructor
+    Matrix<int> def_matrix;
+
+    // Testing matrix with value
+    Matrix<int> value_matrix{ 4, 5, 9 };
+
+    SUCCEED(); // Explicitly mark the test as successful.
 }
 
