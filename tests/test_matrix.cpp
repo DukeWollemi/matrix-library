bea338fdacd65d1fc77d2761d705d4b4a60fdb40
DukeWollemi <omarelshora396@gmail.com>
2026-03-28 10:38:22 +0000

    Forgot to removed unsplit first test.

--- tests/test_matrix.cpp
+++ tests/test_matrix.cpp
@@ -17,20 +17,6 @@ TEST(MatrixDefaultConstructor, CreatesEmptyMatrix) {
     EXPECT_EQ(matrix.data(), nullptr);
 }
 
-TEST(MatrixSizeConstructor, TestNormalAndZero) {
-    Matrix<int> matrix(2,3);
-    EXPECT_EQ(matrix.rows(), 2);
-    EXPECT_EQ(matrix.cols(), 3);
-    EXPECT_EQ(matrix.size(), 6);
-    EXPECT_FALSE(matrix.empty());
-    EXPECT_NE(matrix.data(), nullptr);
-
-    Matrix<int> matrix2(0, 3);
-    EXPECT_EQ(matrix2.size(), 0);
-    EXPECT_TRUE(matrix2.empty());
-    EXPECT_NE(matrix2.data(), nullptr);
-}
-
 TEST(MatrixSizeConstructor, CreatesMatrixWithCorrectShape) {
     Matrix<int> matrix(2, 3);
 
