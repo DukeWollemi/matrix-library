c00e28b33341ff0e93ac5109760f5923172b4362
DukeWollemi <omarelshora396@gmail.com>
2026-03-28 10:32:09 +0000

    Test expects m2 to be null but used not equal assertion.

--- tests/test_matrix.cpp
+++ tests/test_matrix.cpp
@@ -30,3 +30,23 @@ TEST(MatrixSizeConstructor, TestNormalAndZero) {
     EXPECT_TRUE(matrix2.empty());
     EXPECT_NE(matrix2.data(), nullptr);
 }
+
+TEST(MatrixSizeConstructor, CreatesMatrixWithCorrectShape) {
+    Matrix<int> matrix(2, 3);
+
+    EXPECT_EQ(matrix.rows(), 2);
+    EXPECT_EQ(matrix.cols(), 3);
+    EXPECT_EQ(matrix.size(), 6);
+    EXPECT_FALSE(matrix.empty());
+    EXPECT_NE(matrix.data(), nullptr);
+}
+
+TEST(MatrixSizeConstructor, ZeroSizedMatrixHasNoStorage) {
+    Matrix<int> matrix(0, 3);
+
+    EXPECT_EQ(matrix.rows(), 0);
+    EXPECT_EQ(matrix.cols(), 3);
+    EXPECT_EQ(matrix.size(), 0);
+    EXPECT_TRUE(matrix.empty());
+    EXPECT_EQ(matrix.data(), nullptr);
+}
